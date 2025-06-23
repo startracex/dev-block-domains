bb5ca9f9be506a982e1739b24cdbd82a987d1b1a
STARTRACEX <startraceebr@foxmail.com>
2025-06-23 22:11:15 +0800

    first commit

---
+++ domains.js
@@ -0,0 +1,45 @@
+export default [
+  // segmentfault
+  "segmentfault.com",
+
+  // csdn
+  "blog.csdn.net",
+  "wenku.csdn.net",
+  "download.csdn.net",
+  "huaweicloud.csdn.net",
+
+  // gitcode
+  "blog.gitcode.com",
+
+  // jianshu
+  "www.jianshu.com",
+
+  // 51cto
+  "www.51cto.com",
+  "blog.51cto.com",
+
+  // zhihu
+  "zhuanlan.zhihu.com",
+  "www.zhihu.com",
+
+  // imooc
+  "www.imooc.com",
+
+  // docin
+  "www.docin.com",
+
+  // baidu
+  "developer.baidu.com",
+
+  // tencent
+  "cloud.tencent.com",
+
+  // nodejs.cn
+  "nodejs.cn",
+  "p2hp.com",
+  "chartjs.cn",
+  "fenxianglu.cn",
+
+  // trae
+  "www.trae.com.cn",
+];
